3bd7d3c8ab3c0c3f1d1a42515dc467cbe872d7a0
Sam Thomson <samt@samt.st>
2020-07-03 16:30:17 +0100

    write out points to data file

--- points/random-within-country.js
+++ points/random-within-country.js
@@ -1,15 +1,22 @@
 var randomPointsOnPolygon = require('random-points-on-polygon');
 var turf = require('turf');
+var fs = require('fs');
 
 
 const countryGeoJson = require('./out/USA.json')
 const coords = countryGeoJson
 
-var numberOfPoints = 5;
+var numberOfPoints = 100;
 
 var multiGeometry = turf.multiPolygon(coords);
   
 
 var points = randomPointsOnPolygon(numberOfPoints, coords);
 
-console.log(JSON.stringify(points, null, 4));
+const latLonPoints = points.map(point => ({lat: point.geometry.coordinates[1], lng: point.geometry.coordinates[0]}))
+
+console.log(JSON.stringify(latLonPoints, null, 4));
+
+fs.writeFile(`out/random-points.json`, JSON.stringify(latLonPoints), 'utf8', () => {
+	console.log('done')
+});
