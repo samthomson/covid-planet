461b36c1a0259ccad99dcd0078c7c0ea5bac3448
Sam Thomson <samt@samt.st>
2020-07-03 18:09:41 +0100

    rename vars

--- points/lift-country.js
+++ points/lift-country.js
@@ -1,11 +1,20 @@
 var fs = require('fs');
 
 // load data file
-var config = require('./countries.json');
+var countries = require('./countries.json');
 const countryName = 'United States of America'
 
+const uniqueCountries = countries.features.map(country => ({
+	name: country.properties.ADMIN,
+	iso_a2: country.properties.ISO_A2,
+	iso_a3: country.properties.ISO_A3
+}))
+fs.writeFile(`out/all-countries.json`, JSON.stringify(uniqueCountries), 'utf8', () => {
+	console.log('done')
+});
+
 // lift off country we're after
-const needle = config.features.find(feature => feature.properties.ADMIN === countryName)
+const needle = countries.features.find(feature => feature.properties.ADMIN === countryName)
 
 // save it as isolated geojson data file
 fs.writeFile(`out/${needle.properties.ISO_A3}.json`, JSON.stringify(needle), 'utf8', () => {
