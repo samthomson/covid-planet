6273342273672f657418891026fbce669cb8ee3d
Sam Thomson <samt@samt.st>
2020-07-04 16:47:48 +0100

    deadcode--

--- points/random-within-country.js
+++ points/random-within-country.js
@@ -1,17 +1,21 @@
-var fs = require('fs');
+var fs = require("fs");
 
-const DataPointsUtil = require('./data-points-util')
+const DataPointsUtil = require("./data-points-util");
 
-const countryGeoJSONInput = require('./out/RUS.json')
+const countryGeoJSONInput = require("./out/RUS.json");
 
-const latLonPoints = DataPointsUtil.randomPointsWithinGeoJSONCountry(50, countryGeoJSONInput)
+const latLonPoints = DataPointsUtil.randomPointsWithinGeoJSONCountry(
+  50,
+  countryGeoJSONInput
+);
 
 console.log(JSON.stringify(latLonPoints, null, 4));
 
-fs.writeFile(`out/random-points.json`, JSON.stringify(latLonPoints), 'utf8', () => {
-	console.log('done')
-});
-
-
-
-// const testPoints = randomPointsWithinCountry(100, countryGeoJson)
+fs.writeFile(
+  `out/random-points.json`,
+  JSON.stringify(latLonPoints),
+  "utf8",
+  () => {
+    console.log("done");
+  }
+);
