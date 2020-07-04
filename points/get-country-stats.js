adeb3c3e224405b0dc22e6f9f49d07409eb70e65
Sam Thomson <samt@samt.st>
2020-07-04 10:58:00 +0100

    debug option when fetching country stats

--- points/get-country-stats.js
+++ points/get-country-stats.js
@@ -1,12 +1,19 @@
+var fs = require('fs');
 
 const DataPointsUtil = require('./data-points-util')
 
-const getStats = async () => {
+const getStats = async (debug = false) => {
   
 	const stats = await DataPointsUtil.getStatsFromAPI()
 
-	console.log(stats)
-	console.log(stats.length, 'countries with covid data')
+	if (debug) {
+		console.log(stats)
+		console.log(stats.length, 'countries with covid data')
+
+		fs.writeFile(`out/covid-api-data.json`, JSON.stringify(stats), 'utf8', () => {
+			console.log('wrote api data to disk.')
+		});	
+	}
 }
 	
 getStats()
