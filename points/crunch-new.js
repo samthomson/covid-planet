43245026c93068d93c4eaf78d65b8ab9998d5a1e
Sam Thomson <samt@samt.st>
2020-07-04 11:00:43 +0100

    detect missing geo data for corona data

--- points/crunch-new.js
+++ points/crunch-new.js
@@ -1,22 +1,33 @@
 
+// var fs = require('fs');
 
-const geoData = require('./lift-country')
 const DataPointsUtil = require('./data-points-util')
 
 
+const crunch = async () => {
+	// get corona stats per country
+	const coronaStats = await DataPointsUtil.getStatsFromAPI()
+
+	// check we have geo data for all corona country data
+	const coronaStatsCountries = coronaStats.map(stat => stat.CountryCode)
+	const uniqueGeoCountries = DataPointsUtil.getGeoJSONCountriesISOA2()
+	const missing = []
+	coronaStatsCountries.forEach(coronaResult => {
+		if (!uniqueGeoCountries.includes(coronaResult)) {
+			missing.push(coronaResult)
+		}		
+	});
+	if (missing.length > 0) {
+		console.error(missing.length, 'covid data countries with missing geodata: ', missing.join(','))
+	} 
+
+	// for each covid country, generate a some points, and add along with a 24hour count to file
+}
+
+crunch()
 
 
-const uniqueGeoCountries = geoData.getGeoJSONCountriesISOA2()
-console.log(uniqueGeoCountries.length, 'countries with geo polygons data')
-
-const missing = []
-uniqueGeoCountries.forEach(coronaResult => {
-	if (!uniqueGeoCountries.includes(coronaResult)) {
-		missing.push(coronaResult)
-	}
-	
-});
-console.log(missing)
-console.log(missing.length, 'covid data countries with missing geodata')
-
-// for each covid country, generate a some points, and add along with a 24hour count to file
+// const geoCountries = DataPointsUtil.getGeoJSONCountries()
+// fs.writeFile(`out/sanitizedGeoJSON.json`, JSON.stringify(geoCountries, null, 4), 'utf8', () => {
+// 	console.log('done')
+// });	
