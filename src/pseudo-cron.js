524c599a545d0c431ff42ac52ee7bd2d3eef669a
Sam Thomson <samt@samt.st>
2020-07-11 22:17:28 +0100

    midnight each night

--- src/pseudo-cron.js
+++ src/pseudo-cron.js
@@ -2,7 +2,7 @@ const CronJob = require("cron").CronJob;
 
 const crunch = require("./data-generation/util/crunch-new").crunch;
 
-const cronFrequency = "* * * * *";
+const cronFrequency = "0 0 * * *";
 
 console.log("psudeo");
 new CronJob(
