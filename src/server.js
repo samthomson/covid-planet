a3ea9651ec7b50b4b93a9b02ac8e55793c96b058
Sam Thomson <samt@samt.st>
2020-07-11 19:18:54 +0100

    plain express for server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const serve = require("express-static");
 
 const app = express();
 
-app.use(serve(__dirname + "/public"));
+app.use(express.static("./src/public"));
 
 const server = app.listen(8888, function () {
   console.log("server is running at %s", server.address().port);
