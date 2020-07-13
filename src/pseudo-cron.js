8add64a11a214ffb0f9796f81661930088ca8d23
Sam Thomson <samt@samt.st>
2020-07-13 09:16:29 +0100

    cron freq test

--- src/pseudo-cron.js
+++ src/pseudo-cron.js
@@ -3,7 +3,7 @@ const CronJob = require("cron").CronJob;
 const crunch = require("./data-generation/util/crunch-new").crunch;
 
 // const cronFrequency = "0 0 * * *";
-const cronFrequency = "* * * * *"; // every hour, - temp -to eval caching
+const cronFrequency = "*/5 * * * *"; // every hour, - temp -to eval caching
 
 new CronJob(
   cronFrequency,
