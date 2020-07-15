c5b4e6f898f1e2084231336917d10be0d6d7d9c6
Sam Thomson <samt@samt.st>
2020-07-15 19:24:14 +0100

    pregenerate points

---
+++ src/data-generation/generate-points.ts
@@ -0,0 +1,38 @@
+import * as fs from "fs";
+import * as GeoJSONUtil from "./util/geoJSON";
+
+const generate = () => {
+  // get all georegions (sans US and subdivided regions)
+  let regions = GeoJSONUtil.getGeoJSONRegions();
+
+  // generate points within
+  const regionalPoints = regions.map((regionGeoJSON) => {
+    const { feature, regionCode } = regionGeoJSON;
+    // get points
+    const points = GeoJSONUtil.randomPointsWithinGeoJSONCountry(20, feature);
+
+    return {
+      regionCode,
+      points,
+    };
+  });
+
+  // store as json
+  fs.writeFile(
+    `regional-points.json`,
+    JSON.stringify(regionalPoints, null, 4),
+    "utf8",
+    (err) => {
+      console.log("wrote a file to disk containing regional points");
+      const used = process.memoryUsage().heapUsed / 1024 / 1024;
+      console.log(
+        `The script uses approximately ${Math.round(used * 100) / 100} MB`
+      );
+      if (err) {
+        console.error("problem writing file to disk", err);
+      }
+    }
+  );
+};
+
+generate();
