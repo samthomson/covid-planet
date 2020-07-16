0f0b74340772280b64d752a67bade622ae84c07b
Sam Thomson <samt@samt.st>
2020-07-16 11:33:36 +0100

    test diff cron expression

--- src/pseudo-cron.js
+++ src/pseudo-cron.js
@@ -2,7 +2,8 @@ const CronJob = require("cron").CronJob;
 
 const crunch = require("./data-generation/util/crunch-new").crunch;
 
-const cronFrequency = "0 */6 * * *";
+// const cronFrequency = "0 0,6,12,18 * * *";
+const cronFrequency = "0,10,20,30,40,50 * * * *";
 
 new CronJob(
   cronFrequency,
