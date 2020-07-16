9c718b61a657941055f7a010d1b6b74c0298d13e
Sam Thomson <samt@samt.st>
2020-07-16 14:01:04 +0100

    cron tweeks

--- src/pseudo-cron.ts
+++ src/pseudo-cron.ts
@@ -3,11 +3,14 @@ import { CronJob } from "cron";
 import { crunch } from "./data-generation/util/crunch-new";
 
 // const cronFrequency = "0 0,6,12,18 * * *";
-const cronFrequency = "0,10,20,30,40,50 * * * *";
+// const cronFrequency = "0,10,20,30,40,50 * * * *";
+const cronFrequency = "0 * * * * *";
 
+// console.log("cron\n\n");
 new CronJob(
   cronFrequency,
   () => {
+    // console.log(new Date().toUTCString() + "crunch\n");
     crunch();
   },
   undefined,
