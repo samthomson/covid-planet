66c53fe8087186e1287dacf68d11a412ea4fa17d
Sam Thomson <samt@samt.st>
2020-07-16 14:20:25 +0100

    cron frequency > four times a day

--- src/pseudo-cron.ts
+++ src/pseudo-cron.ts
@@ -2,16 +2,11 @@ import { CronJob } from "cron";
 
 import { crunch } from "./data-generation/util/crunch-new";
 
-// const cronFrequency = "0 0,6,12,18 * * *";
-// const cronFrequency = "0 */6 * * *";
-const cronFrequency = "*/6 * * * *";
-// const cronFrequency = "0 * * * * *";
+const cronFrequency = "0 */6 * * *";
 
-// console.log("cron\n\n");
 new CronJob(
   cronFrequency,
   () => {
-    // console.log(new Date().toUTCString() + "crunch\n");
     crunch();
   },
   undefined,
